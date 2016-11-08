e63449e1ff07f3b650d9ea1c6435e86d0318a36b
Franco Sarachu <fsarachu@gmail.com>
2016-11-08 15:48:20 -0300

    Make instance attributes class attributes

--- handlers/Handler.py
+++ handlers/Handler.py
@@ -5,10 +5,8 @@ import jinja2
 
 
 class Handler(webapp2.RequestHandler):
-    def __init__(self):
-        super().__init__()
-        self._template_dir = os.path.join(os.path.dirname(__file__), '../templates')
-        self._jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(self._template_dir), autoescape=True)
+    TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../templates')
+    JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR), autoescape=True)
 
     def write(self, *args, **kwargs):
         self.response.write(*args, **kwargs)
