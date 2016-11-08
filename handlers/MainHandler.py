2be2d2f99609ce1c69525c6e31ae1e38921fb617
Franco Sarachu <fsarachu@gmail.com>
2016-11-08 17:47:32 -0300

    Print latest submitted art in mainpage

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -1,6 +1,9 @@
+from google.appengine.ext import db
 from Handler import Handler
 
 
 class MainHandler(Handler):
     def get(self):
-        self.render('index.html')
+        arts = db.GqlQuery("SELECT * FROM Art "
+                           "ORDER BY created DESC")
+        self.render('index.html', arts=arts)
