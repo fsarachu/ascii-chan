9f224f3e0630d2499af6f30cfb59341dfce124c2
Franco Sarachu <fsarachu@gmail.com>
2016-11-08 15:15:37 -0300

    Define __init__ with jinja2 templates attributes

--- handlers/Handler.py
+++ handlers/Handler.py
@@ -1,6 +1,14 @@
+import os
+
 import webapp2
+import jinja2
 
 
 class Handler(webapp2.RequestHandler):
+    def __init__(self):
+        super().__init__()
+        self._template_dir = os.path.join(os.path.dirname(__file__), '../templates')
+        self._jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(self._template_dir), autoescape=True)
+
     def write(self, *args, **kwargs):
         self.response.write(*args, **kwargs)
